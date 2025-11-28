ebb9fefa514cba1bfa0b1446cd7b1d05544bd2ba
TusharRoy23 <risenboy23@gmail.com>
2025-11-28 14:44:26 +0100

    health checkup

--- src/modules/reviewer/controller/reviewer.controller.ts
+++ src/modules/reviewer/controller/reviewer.controller.ts
@@ -2,21 +2,26 @@ import { inject } from "inversify";
 import type { Request, Response } from "express";
 import { TYPES } from "../../../config/types.ts";
 import type { IReviewerService } from "../interface/IReviewer.service.ts";
-import { controller, httpPost, requestBody } from "inversify-express-utils";
+import { controller, httpGet, httpPost, requestBody } from "inversify-express-utils";
 import type { ReviewPayloadType } from "../dto/review-payload.dto.ts";
 import { VerifyGitHubOIDCMiddleware } from "../../../middleware/github-oidc.middleware.ts";
 
-@controller("/review", VerifyGitHubOIDCMiddleware)
+@controller("/review")
 export class ReviewerController {
     constructor(
         @inject(TYPES.IReviewerService) private readonly reviewerService: IReviewerService
     ) { }
 
-    @httpPost("/")
+    @httpPost("/", VerifyGitHubOIDCMiddleware)
     async performCodeReview(
         @requestBody() payload: ReviewPayloadType, req: Request, res: Response
     ) {
         const result = await this.reviewerService.requestForReview(payload);
         return res.status(201).json({ data: result });
+    }
+
+    @httpGet("/health")
+    async healthCheckup(req: Request, res: Response) {
+        return res.status(200).json({ data: 'I am Good' })
     }
 }
