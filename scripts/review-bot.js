604fe6fb7b63c0b5082f2317e1cdddbe73eb771f
TusharRoy23 <risenboy23@gmail.com>
2025-11-28 16:28:56 +0100

    checking logs

--- scripts/review-bot.js
+++ scripts/review-bot.js
@@ -83,8 +83,6 @@ async function main() {
     const result = await apiClient.post(`/review`, {
       code: escapedDiff
     });
-    console.log('result: ', result);
-
     console.log(JSON.stringify(result.data || []));
 
   } catch (error) {
